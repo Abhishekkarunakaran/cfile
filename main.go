e6d1628fbda27f4964aa83be42291d3169862fc1
Abhishek K K <abhishekkarunakaran100@gmail.com>
2025-05-29 07:33:05 +0530

    replaced the os.Exit(0) with return

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("File name required.")
 		fmt.Println("Usage: cfile <filename>")
-		os.Exit(0)
+		return
 	}
 	fileName := os.Args[1]
 
@@ -20,12 +20,12 @@ func main() {
 	if err != nil {
 		fmt.Println("No such file exists.")
 		fmt.Println("Enter a valid file name.")
-		os.Exit(0)
+		return
 	}
 
 	if err := clipboard.WriteAll(string(data)); err != nil {
 		fmt.Println("Failed to copy to clipboard")
 		fmt.Println("Try again.")
-		os.Exit(0)
+		return
 	}
 }
